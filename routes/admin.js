1e4727cfe710554c8c60fce174196cdf9daa3a08
BreakTheCage <sarbaztanha707@outlook.com>
2019-07-29 21:35:03 +0400

    #67;

--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,16 @@ const router = express.Router();
 
 router.use('/favicon.ico' ,(req, res, next) => { });
 
+// /admin/add-product => GET
 router.get('/add-product' ,(req, res, next) => {
-    console.log("Inside '/add-product' route!");
-    res.send('<form method="POST" action="product"><input type="text" name="title" ><button type="submit" >Add Product</button></form>');
+    console.log("Inside 'GET: admin/add-product' route!");
+    res.send('<form method="POST" action="/admin/add-product"><input type="text" name="title" ><button type="submit" >Add Product</button></form>');
 });
 
-router.post('/product' ,(req, res, next) => {
-    console.log("Inside '/product' route!");
-    console.log("Inside '/product' route! => request body: ",req.body);
+// /admin/add-product => POST
+router.post('/add-product' ,(req, res, next) => {
+    console.log("Inside 'POST: admin/add-product' route!");
+    console.log("Inside 'POST: admin/add-product' route! => request body: ",req.body);
     res.redirect('/')
 });
 
