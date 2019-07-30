13c9e344a8b057bebcdd99f42ddacef2390adf72
BreakTheCage <sarbaztanha707@outlook.com>
2019-07-30 22:23:22 +0400

    #dfjg

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(shopRoutes);
 
 //Page NOt Found
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404');
 });
 app.listen(3000);
