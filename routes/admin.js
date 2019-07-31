e33babcfb1d49c0bfc70e5191ee7c53bb41db435
BreakTheCage <sarbaztanha707@outlook.com>
2019-07-31 12:48:25 +0400

    #90; Wrap Up

--- routes/admin.js
+++ routes/admin.js
@@ -9,21 +9,14 @@ router.get("/add-product", (req, res, next) => {
   // /admin/add-product => GET
   console.log("Inside 'GET: admin/add-product' route!");
 
-  // res.render("add-product", {
-  //   pageTitle: "Add Product",
-  //   label: "Insert Your Favorit Book",
-  //   path: "/admin/add-product"
-  // });//============PUG
-
-  res.render("add-product", {
+  res.render("add-product", { //refere to name of file to render inside views folder
     pageTitle: "Add Product",
     label: "Insert Your Favorit Book",
     path: "/admin/add-product",
-    // layout: false, //Special Key: very important
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
-  }); //EJS and handlebars
+  }); 
 });
 router.post("/add-product", (req, res, next) => {
   // /admin/add-product => POST
