54bab70ce534a2dd28cd5ee1096eee66a7023486
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-08-02 13:44:51 +0400

    #106; Registering the route

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const path = require("path");
-const adminController = require('../controllers/admin');
+const adminController = require("../controllers/admin");
 const router = express.Router();
 
 router.use("/favicon.ico", (req, res, next) => {});
 
 router.get("/add-product", adminController.getAddProduct);
-router.post("/add-product", adminController.postAddProduct);
 
 //get: Admin Products
-router.get("/products");
+router.get("/products", adminController.getProducts);
+
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
